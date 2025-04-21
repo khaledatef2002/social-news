8445b04626a28784acd90492426d87210d9d7f65
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-04-21 15:01:07 +0200

    no articles case

--- resources/views/components/article-summary-list.blade.php
+++ resources/views/components/article-summary-list.blade.php
@@ -1,3 +1,7 @@
-@foreach ($articles as $article)
-    <x-article-summary-list-item :article="$article" />
-@endforeach
+@if ($articles->count() == 0)
+    @foreach ($articles as $article)
+        <x-article-summary-list-item :article="$article" />
+    @endforeach
+@else
+    <p class="fw-bold mb-0 fs-5">لا يوجد نتائج اخرى</p>
+@endif
