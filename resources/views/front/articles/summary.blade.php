404a89d2ea4a03de69a4e5d6438bb6371cd8b7dd
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-04-26 20:17:17 +0300

    Update summary.blade.php

--- resources/views/front/articles/summary.blade.php
+++ resources/views/front/articles/summary.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="home-wraper flex-fill d-flex justify-content-between py-4">
         <main class="articles col-12 d-flex flex-wrap justify-content-center" data-article-type="summary">
+            <div class="w-100 d-flex justify-content-center mb-3">
+                <h3 class="align-self-start mx-auto d-block"><i class="fas fa-cut fs-4"></i> مختصر المقالات</h3>
+            </div>
             <div class="row w-100">
-                <div class="w-100 d-flex justify-content-center mb-3">
-                    <h3 class="align-self-start mx-auto d-block"><i class="fas fa-cut fs-4"></i> مختصر المقالات</h3>
-                </div>
                 <x-article-summary-list :articles="$first_articles" />
                 <div class="getingArticlesLoader justify-content-center w-100">
                     <span class="loader"></span>
