88696b84b3305d28c9cd2ade57619ecac2341407
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-04-26 20:52:26 +0300

    .

--- app/Services/ArticleService.php
+++ app/Services/ArticleService.php
@@ -9,7 +9,7 @@ class ArticleService
     public function get_articles($last_article_id = null, $limit = 20)
     {
         $articles = Article::orderByDesc('id')->when($last_article_id, function($query) use ($last_article_id) {
-            return $query->where('id', '>', $last_article_id);
+            return $query->where('id', '<', $last_article_id);
         })->limit($limit)->get();
 
         return $articles;
--- app/Services/TvArticlesService.php
+++ app/Services/TvArticlesService.php
@@ -8,8 +8,8 @@ class TvArticlesService
 {
     public function get_tv_articles($last_article_id = null, $limit = 20)
     {
-        $articles = TvArticle::orderBy('id')->when($last_article_id, function($query) use ($last_article_id) {
-            return $query->where('id', '>', $last_article_id);
+        $articles = TvArticle::orderByDesc('id')->when($last_article_id, function($query) use ($last_article_id) {
+            return $query->where('id', '<', $last_article_id);
         })->limit($limit)->get();
 
         return $articles;
