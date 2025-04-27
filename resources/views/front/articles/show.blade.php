56364b5b8fa23c270dcef024eac0a559e2514024
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-04-27 22:32:33 +0300

    ز

--- resources/views/front/articles/show.blade.php
+++ resources/views/front/articles/show.blade.php
@@ -22,7 +22,7 @@
 
 @section('content')
 <main class="py-3 col-12 d-flex flex-column" data-article-type="summary">
-    <article class="card rounded border-0 shadow-sm" data-article-id="{{ $article->id }}">
+    <article class="card col-lg-7 col-md-8 col-12 mx-autorounded border-0 shadow-sm" data-article-id="{{ $article->id }}">
         <div class="py-2 px-2 text-start">
             <div class="meta-content d-flex flex-column mb-2 fw-bold">
                 <div class="d-flex justify-content-between align-items-center">
