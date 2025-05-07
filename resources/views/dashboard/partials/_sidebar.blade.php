a0bd157472888f8d90d8068f33e3a77329965096
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-05-07 21:26:43 +0300

    side bar permissions

--- resources/views/dashboard/partials/_sidebar.blade.php
+++ resources/views/dashboard/partials/_sidebar.blade.php
@@ -35,59 +35,69 @@
                         <i class="ri-home-3-fill"></i> <span>@lang('dashboard.home')</span>
                     </a>
                 </li> <!-- End Home Menu-->
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.articles-category.index' ? 'active' : ''}}" href="#articlesMenu" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarUI">
-                        <i class="ri-article-line"></i> <span>@lang('dashboard.articles')</span>
-                    </a>
-                    <div class="collapse menu-dropdown mega-dropdown-menu" id="articlesMenu">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="{{ route('dashboard.articles-categories.index') }}" class="nav-link">@lang('dashboard.categories')</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="{{ route('dashboard.articles.index') }}" class="nav-link">@lang('dashboard.all-articles')</a>
-                                    </li>
-                                </ul>
+                @if (Auth::user()->hasPermissionTo('articles_show'))
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.articles-category.index' ? 'active' : ''}}" href="#articlesMenu" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarUI">
+                            <i class="ri-article-line"></i> <span>@lang('dashboard.articles')</span>
+                        </a>
+                        <div class="collapse menu-dropdown mega-dropdown-menu" id="articlesMenu">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <ul class="nav nav-sm flex-column">
+                                        <li class="nav-item">
+                                            <a href="{{ route('dashboard.articles-categories.index') }}" class="nav-link">@lang('dashboard.categories')</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="{{ route('dashboard.articles.index') }}" class="nav-link">@lang('dashboard.all-articles')</a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.tv-articles-category.index' ? 'active' : ''}}" href="#tvArticlesMenu" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarUI">
-                        <i class="ri-tv-line"></i> <span>@lang('dashboard.tv-articles')</span>
-                    </a>
-                    <div class="collapse menu-dropdown mega-dropdown-menu" id="tvArticlesMenu">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="{{ route('dashboard.tv-articles-categories.index') }}" class="nav-link">@lang('dashboard.categories')</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="{{ route('dashboard.tv-articles.index') }}" class="nav-link">@lang('dashboard.all-tv-articles')</a>
-                                    </li>
-                                </ul>
+                    </li>
+                @endif
+                @if (Auth::user ()->hasPermissionTo('tv_articles_show'))         
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.tv-articles-category.index' ? 'active' : ''}}" href="#tvArticlesMenu" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarUI">
+                            <i class="ri-tv-line"></i> <span>@lang('dashboard.tv-articles')</span>
+                        </a>
+                        <div class="collapse menu-dropdown mega-dropdown-menu" id="tvArticlesMenu">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <ul class="nav nav-sm flex-column">
+                                        <li class="nav-item">
+                                            <a href="{{ route('dashboard.tv-articles-categories.index') }}" class="nav-link">@lang('dashboard.categories')</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="{{ route('dashboard.tv-articles.index') }}" class="nav-link">@lang('dashboard.all-tv-articles')</a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.users.index' ? 'active' : ''}}" href="{{ route('dashboard.users.index') }}" role="button">
-                        <i class="ri-user-fill"></i> <span>@lang('dashboard.users')</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.roles.index' ? 'active' : ''}}" href="{{ route('dashboard.roles.index') }}" role="button">
-                        <i class="ri-key-2-fill"></i> <span>@lang('dashboard.roles')</span>
-                    </a>
-                </li> <!-- End Website Settings Menu -->
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.website_setting.index' ? 'active' : ''}}" href="{{ route('dashboard.website_setting.index') }}" role="button">
-                        <i class="ri-tools-fill"></i> <span>@lang('dashboard.website-settings')</span>
-                    </a>
-                </li> <!-- End Website Settings Menu -->
+                    </li>
+                @endif
+                @if (Auth::user()->hasPermissionTo('users_show'))
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.users.index' ? 'active' : ''}}" href="{{ route('dashboard.users.index') }}" role="button">
+                            <i class="ri-user-fill"></i> <span>@lang('dashboard.users')</span>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->hasPermissionTo('roles_show'))
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.roles.index' ? 'active' : ''}}" href="{{ route('dashboard.roles.index') }}" role="button">
+                            <i class="ri-key-2-fill"></i> <span>@lang('dashboard.roles')</span>
+                        </a>
+                    </li> <!-- End Website Settings Menu -->
+                @endif
+                @if (Auth::user()->hasPermissionTo('website_settings_show'))
+                    <li class="nav-item">               
+                        <a class="nav-link menu-link {{ Route::currentRouteName() ==  'dashboard.website_setting.index' ? 'active' : ''}}" href="{{ route('dashboard.website_setting.index') }}" role="button">
+                            <i class="ri-tools-fill"></i> <span>@lang('dashboard.website-settings')</span>
+                        </a>
+                    </li> <!-- End Website Settings Menu -->
+                @endif
             </ul>
         </div>
         <!-- Sidebar -->
